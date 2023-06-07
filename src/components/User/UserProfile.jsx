b874b27a23a711196d1a9aedfc090ae1c0c5bfe2
bluelsa <elsa22745@gmail.com>
2023-06-07 18:27:34 +0800

    Feat: add profile edit popup

--- src/components/User/UserProfile.jsx
+++ src/components/User/UserProfile.jsx
@@ -1,15 +1,21 @@
 import styles from './User.module.scss'
+import EditPopup from './EditPopup';
+import { useState } from 'react'
 
 const UserProfile = () => {
+
+  const [ editPopup, setEditPopup ] = useState(false)
+
   return (
-    <div className={styles.Container}>
+    <div className={styles.profileContainer}>
       <img
         className={styles.cover}
         src="https://picsum.photos/639/200"
         alt="cover"
       />
       <img className={styles.avatar} src="https://i.pravatar.cc" alt="avatar" />
-      <button className={styles.editButton}>編輯個人資料</button>
+      <button className={styles.editButton} onClick={()=> setEditPopup(true)}>編輯個人資料</button>
+      <EditPopup trigger={editPopup} setTrigger={setEditPopup} />
       <div className={styles.infoDescription}>
         <div className={styles.name}>John Doe</div>
         <div className={styles.account}>@heyjohn</div>
