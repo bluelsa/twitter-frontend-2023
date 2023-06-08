af5830d72dc4eaf748a7c07de9dfb49ef883d3a5
u418572@kimo.com <u418572@kimo.com>
2023-06-08 13:55:06 +0800

    feat: navbar連結功能

--- src/common/NavBar.jsx
+++ src/common/NavBar.jsx
@@ -1,12 +1,22 @@
-import styles from "../pages/HomeStyle.module.scss";
+// import styles from "../pages/HomeStyle.module.scss";
+import styles from "./NavBarStyles.module.scss";
 import { ReactComponent as Logo } from "../assets/image/ac-logo.svg";
-import { ReactComponent as NavIndex } from "../assets/image/NavIndex.svg";
+// import { ReactComponent as NavIndex } from "../assets/image/NavIndex.svg";
 import { ReactComponent as NavPersonal } from "../assets/image/NavPersonal.svg";
 import { ReactComponent as NavSetting } from "../assets/image/NavSetting.svg";
 import { ReactComponent as NavOut } from "../assets/image/NavOut.svg";
 import { ReactComponent as NavButton } from "../assets/image/NavButton.svg";
+import { ReactComponent as BlackIndex } from "../assets/image/NavBlackHouse.svg";
+import { Link } from "react-router-dom";
+// import clsx from "clsx";
+// import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+// `${styles.blueHouse}`;
+const NavBar = (props) => {
+  const [house, setHouse] = useState(true);
+  const [person, setPerson] = useState(false);
+  const [setting, setSetting] = useState(false);
 
-const NavBar = () => {
   return (
     <div className={styles.leftColumn}>
       <nav className={styles.navContainer}>
@@ -14,12 +24,47 @@ const NavBar = () => {
           <Logo className={styles.interaction} />
         </div>
         <div className={styles.navBar}>
-          <NavIndex className={styles.interaction} />
-          <NavPersonal className={styles.interaction} />
-          <NavSetting className={styles.interaction} />
+          <Link to="/"></Link>
+          <BlackIndex
+            id="1"
+            onClick={() => {
+              props.setHouse(true);
+              props.setPerson(false);
+              props.setSetting(false);
+            }}
+            className={
+              props.house ? `${styles.orangeIcon}` : `${styles.blackIcon}`
+            }
+          />
+          <Link to="/user">
+            <NavPersonal
+              id="2"
+              onClick={() => {
+                props.setHouse(false);
+                props.setPerson(true);
+                props.setSetting(false);
+              }}
+              className={
+                props.person ? `${styles.orangeIcon}` : `${styles.blackIcon}`
+              }
+            />
+          </Link>
+          <Link to="/setting">
+            <NavSetting
+              id="3"
+              onClick={() => {
+                props.setHouse(false);
+                props.setPerson(false);
+                props.setSetting(true);
+              }}
+              className={
+                props.setting ? `${styles.orangeIcon}` : `${styles.blackIcon}`
+              }
+            />
+          </Link>
+
           <NavButton className={styles.interaction} />
         </div>
-
         <NavOut className={styles.logOut} />
       </nav>
     </div>
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -2,13 +2,25 @@ import styles from "./HomeStyle.module.scss";
 import NavBar from "../common/NavBar";
 import Popular from "../common/Popular";
 import ReplyTwits from "../common/Reply/ReplyTwits";
+import { useState, useEffect } from "react";
+// import {useState} from "react"
 // import Twits from './Twits';
 
 const MainPage = () => {
+   const [house, setHouse] = useState(true);
+   const [person, setPerson] = useState(false);
+   const [setting, setSetting] = useState(false);
   return (
     <div className={styles.homeContainer}>
       <div className={styles.mainContainer}>
-        <NavBar />
+        <NavBar 
+          house={house}
+          setHouse={setHouse}
+          person={person}
+          setPerson={setPerson}
+          setting={setting}
+          setSetting={setSetting}
+        />
         <ReplyTwits />
         {/* <Twits /> */}
         <Popular />
